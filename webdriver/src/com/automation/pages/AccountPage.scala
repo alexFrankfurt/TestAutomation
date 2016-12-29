74454f2eaef102bd37ad717b138fa84a6f304cc6
Alexander <alex_frankfurt@mail.ru>
2016-12-29 12:37:47 +0300

    Update AccountPage.scala

--- webdriver/src/com/automation/pages/AccountPage.scala
+++ webdriver/src/com/automation/pages/AccountPage.scala
@@ -1,1 +1,26 @@
 
+package com.automation.pages
+
+import java.util.concurrent.TimeUnit
+
+import org.apache.logging.log4j.{LogManager, Logger}
+import org.openqa.selenium.{WebDriver, WebElement}
+import org.openqa.selenium.support.{FindBy, PageFactory}
+
+
+class AccountPage(driver: WebDriver){
+
+  private val logger: Logger = LogManager.getRootLogger
+
+  val BaseUrl = "https://www.vocabulary.com/account/"
+  
+  @FindBy(xpath = "//*[@id='pageContent']/div/div/div/nav/ul/li[1]/a")
+  var accINfField: WebElement = null
+  
+  PageFactory.initElements(this.driver, this)
+  
+  def check(): Boolean = {
+    accINfField.getText == "Account Information"
+  }
+  
+}
