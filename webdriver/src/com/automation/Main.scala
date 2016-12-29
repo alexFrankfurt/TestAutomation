d22a615189103379012b6f68077eba8251c3c661
Alexander <alex_frankfurt@mail.ru>
2016-12-29 12:34:06 +0300

    Update Main.scala

--- webdriver/src/com/automation/Main.scala
+++ webdriver/src/com/automation/Main.scala
@@ -2,12 +2,15 @@ package com.automation
 
 import com.automation.steps.Steps
 import org.testng.annotations.{AfterMethod, BeforeMethod, Test}
+import org.testng.Assert
 
 class Main {
 
   val UserEmail = "alex_frankfurt@mail.ru"
   val UserPassword = "might39"
   val InvalidUserPassword = "fjldksjfl"
+  val SignInText = "Sign in to Vocabulary.com"
+  val UserName = "Alex A."
 
   var steps: Steps = null
 
@@ -17,23 +20,36 @@ class Main {
   }
 
 //  @Test
-  def openSignIn() = steps.openSignIn()
+  def openSignIn() = {
+    steps.openSignIn()
+    Assert.assertTrue(steps.isLoginPage(SignInText)
+  }
 
 //  @Test
-  def login() = steps.signIn(UserEmail, UserPassword)
+  def login() = {
+    steps.signIn(UserEmail, UserPassword)
+    Assert.assertTrue(steps.loggedIn(Username))
+  }
 
 
 //  @Test
-  def incorrectLogin = steps.incorrectSignIn(UserEmail, InvalidUserPassword)
+  def incorrectLogin = {
+    steps.incorrectSignIn(UserEmail, InvalidUserPassword)
+    Assert.assertTrue(steps.incorrectLogInMessage())
+  }
 
 
 //  @Test
-  def openSignUp() = steps.openSignUp()
+  def openSignUp() = {
+    steps.openSignUp()
+    Assert.assertTrue(steps.isSignUpForm())
+  }
 
   @Test
   def openAccount() = {
     steps.signIn(UserEmail, UserPassword)
     steps.accountLookUp
+    Assert.assertTrue(steps.isAccauntPage())
   }
 
   @AfterMethod(description = "Stop Browser")
